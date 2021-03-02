8dc19db13a9fd4d28ddfdc96b66869373d617611
DESKTOP-37G8I0R\Benjamin <1@zmail.top>
2021-03-02 17:27:48 +0800

    fix:修改路由

--- src/common/routes.js
+++ src/common/routes.js
@@ -3,8 +3,8 @@ import home from '../pages/home.vue';
 
 
 let options = [
-  {path: '/index', redirect: '/index'},
-  {path: '/', name: 'home', component: home},
+  {path: '/', redirect: '/index'},
+  {path: '/home', name: 'home', component: home},
   {path: '/index', name: 'index', component: index}
 ];
 export default options;
